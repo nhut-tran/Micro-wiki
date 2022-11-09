c567b1e82731bc1b6dd609b82d9258a699874987
nhut-tran <trannhut1707@gmail.com>
2022-11-09 20:51:16 +0700

    change email config

--- src/utils/email.js
+++ src/utils/email.js
@@ -3,22 +3,22 @@ const path = require('path')
 class SendEmail {
     constructor(user, url) {
         this.to = user.email,
-        this.from = 'Micro-wiki <trannhut1707@gmail.com>'
+            this.from = 'Micro-wiki <trannhut0717@gmail.com>'
         this.url = url
     }
 
     createTransport() {
-       return nodeMailer.createTransport({
-           service: 'SendGrid',
-           auth: {
-               user: 'apikey',
-               pass: process.env.EMAIL_PASS
-           }
-       })
+        return nodeMailer.createTransport({
+            service: 'SendGrid',
+            auth: {
+                user: 'apikey',
+                pass: process.env.EMAIL_PASS
+            }
+        })
     }
 
-   async send(template, subject) {
-       const options = {
+    async send(template, subject) {
+        const options = {
             from: this.from,
             to: this.to,
             subject,
@@ -28,8 +28,8 @@ class SendEmail {
                 path: path.join(__dirname + '../../../public/img/logo.png'),
                 cid: 'logo@cid'
             }]
-       }
-       await this.createTransport().sendMail(options)
+        }
+        await this.createTransport().sendMail(options)
     }
 }
 
